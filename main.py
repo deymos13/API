3dac2132056f4e7fadefc11c54df77273531a2db
deymos13 <161309073+deymos13@users.noreply.github.com>
2024-04-10 15:04:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ api_url = "https://randomuser.me/api/"
 response = requests.get(api_url)   # Отправляем GET-запрос и сохраняем ответ в переменной response
 if response.status_code == 200:    # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
     response_dict = response.json()
-    result_data = response_dict["result"]
-    dat1 = result_data['results'][0]["gender"]
-    dat2 = result_data['results'][0]["name"]["first"]
-    dat3 = result_data['results'][0]["name"]["last"]
+    dat1 = response_dict['results'][0]["gender"]
+    dat2 = response_dict['results'][0]["name"]["first"]
+    dat3 = response_dict['results'][0]["name"]["last"]
     print("Гендер", dat1)
     print("Имя", dat2)
     print("Фамилия", dat3)
